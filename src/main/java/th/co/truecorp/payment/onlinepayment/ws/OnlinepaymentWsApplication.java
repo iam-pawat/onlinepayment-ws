d4404063451ee9a40253cdc48143be733f5ef99e
pawat iamsupa <pawat_iam@truecorp.co.th>
2018-04-28 17:50:38 +0700

    init

---
+++ src/main/java/th/co/truecorp/payment/onlinepayment/ws/OnlinepaymentWsApplication.java
@@ -0,0 +1,12 @@
+package th.co.truecorp.payment.onlinepayment.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinepaymentWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinepaymentWsApplication.class, args);
+	}
+}
